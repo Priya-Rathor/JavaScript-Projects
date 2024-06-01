809979f6832d5bbe539ee37440df14ea80088d2c
Priya Rathor <146440973+Priya-Rathor@users.noreply.github.com>
2024-06-01 08:19:56 +0530

    Add files via upload

---
+++ Dom6/script.js
@@ -0,0 +1,16 @@
+var bulb=document.querySelector("#bulb")
+var btn=document.querySelector("button")
+var flag=0
+
+btn.addEventListener("click",function(){
+if(flag==0){ 
+       bulb.style.backgroundColor="yellow"
+    flag=1         
+}else{
+    bulb.style.backgroundColor="skyblue"
+    flag=0
+
+}
+
+
+})
